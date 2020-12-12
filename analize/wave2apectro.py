482c7527a708a0be4b0fbf5ce9373796be92cb51
Mukai Ven Kyoshiro <ven.kyoshiro@gmail.com>
2020-12-13 08:38:43 +0900

    logscale spectro

--- analize/wave2apectro.py
+++ analize/wave2apectro.py
@@ -2,13 +2,18 @@
 
 import scipy.io.wavfile as wio
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 if __name__ == '__main__':
     rate, data = wio.read("out.wav")
     fig,ax = plt.subplots(2,1,sharex=True,figsize=(16,9))
-    pxx, freq, bins, t = ax[0].specgram(data[:,0],Fs = rate,cmap='inferno')
-    pxx, freq, bins, t = ax[1].specgram(data[:,1],Fs = rate,cmap='inferno')
+    pxx, freqs, bins, t = ax[0].specgram(data[:,0],Fs = rate)
+    ax[0].pcolormesh(bins, freqs, 10*np.log10(pxx),cmap='magma')
+    ax[0].set_yscale('symlog', linthreshy=0.01)
+    pxx, freqs, bins, t = ax[1].specgram(data[:,1],Fs = rate)
+    ax[1].pcolormesh(bins, freqs, 10*np.log10(pxx),cmap='magma')
+    ax[1].set_yscale('symlog', linthreshy=0.01)
     ax[1].set_xlabel('sec')
     ax[0].set_ylabel('Hz')
     ax[1].set_ylabel('Hz')
